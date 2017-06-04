005311aa106a1f6ea229679e0b220ba9cae93399
maningning <727039390@qq.com>
2017-06-04 22:56:33 +0800

    project start

---
+++ zhifou/src/main/java/com/weasp/ZhifouApplication.java
@@ -0,0 +1,13 @@
+package com.weasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhifouApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ZhifouApplication.class, args);
+	}
+}
