8b7cb603a7c460676c835791bd763fabe42f3ca7
hellojackhui <HUIJIAWEI@hotmail.com>
2017-06-16 00:51:52 +0800

    0616branch

---
+++ zhifou/src/main/java/com/weasp/controller/MessageController.java
@@ -0,0 +1,7 @@
+package com.weasp.controller;
+
+/**
+ * Created by JackHui on 2017/6/15.
+ */
+public class MessageController {
+}
---
+++ zhifou/src/main/java/com/weasp/service/CommentService.java
@@ -0,0 +1,7 @@
+package com.weasp.service;
+
+/**
+ * Created by JackHui on 2017/6/15.
+ */
+public class CommentService {
+}
